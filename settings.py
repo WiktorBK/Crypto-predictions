440ce2ea826849b1b75e309b3aa41caa9410bc8d
wiktor bozek <wiktorsebbozek@gmail.com>
2023-07-31 15:28:55 +0200

    Create config file

---
+++ settings.py
@@ -0,0 +1,14 @@
+SYMBOL = "ETHUSDT"
+INTERVAL = "60"
+MONTHS = 28
+EMA1 = 200 # long term ema window
+EMA2 = 20 # short term ema window
+
+TP_RATIO = 1.5 # takeprofit percentage
+
+
+CAPITAL_USD = 100
+FEE_RATE = 0.0006
+LVG = 15
+
+
